d9f74a6fddb6b5f150ac18bc019b7e432afee2dd
Fernandesmtc91 <126025283+Fernandesmtc91@users.noreply.github.com>
2025-08-25 00:18:05 -0300

    modificacoes com blade

--- src/resources/views/hellowordl.blade.php
+++ src/resources/views/hellowordl.blade.php
@@ -1,3 +1,13 @@
 <h1>{{ 'Hello World!' }}</h1>
 <p>{{ 'Bem-vindo ao Laravel!' }}</p>
 <a href="/">voltar para home do laravel</a>
+
+@if(1>4) <!-- Códigos em diretivas do blade -->
+ <p> a condicao e true</p>
+@else
+ <p> a condicao e false</p>
+@endif
+
+<p>{{$nome}}</p>
+<p>{{$idade}}</p>
+<p> o nome dele e {{$nome}} e tem {{$idade}} anos de idade e a profissao dele e {{$profissao}}</p>
--- src/routes/web.php
+++ src/routes/web.php
@@ -3,7 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/hl', function () {
-    return view('hellowordl');
+
+    $nome = 'Francisco';
+    $idade = 30;
+    $profissao = 'programador';
+    return view('hellowordl',['nome' => $nome, 'idade' => $idade,'profissao' => $profissao]);
 });
 Route::get('/', function () {
     return view('welcome');
